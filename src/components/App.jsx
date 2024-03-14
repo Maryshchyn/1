6d68625725e99897ee7c03bf2fd65f230f02abad
Maryshchyn <maryshchyn89@gmail.com>
2024-03-14 07:58:18 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,12 @@
-export const App = () => {
-  console.log('4')
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+
+export class App extends Component {
+  state = {} 
   
-};
+
+  
+  render() { 
+    return (<></>);
+  }
+}
+
